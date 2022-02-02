074414ff1d17f3a25777f91c58e30483d2254dab
111qqz <111qqz@users.noreply.github.com>
2022-02-02 09:13:17 +0000

    add goodbye 2021 111qqz/hugo-blog@89d3e1ea4c543ac72b54010f87f0a10400fe1dc5

--- js/hux-blog.js
+++ js/hux-blog.js
@@ -40,14 +40,12 @@ jQuery(document).ready(function($) {
 
     //primary navigation slide-in effect
     if ($(window).width() > MQL) {
-            var headerHeight = $('.navbar-custom').height(),
-        bannerHeight = $('.intro-header .container').height();
+        var headerHeight = $('.navbar-custom').height();
         $(window).on('scroll', {
                 previousTop: 0
             },
             function() {
-                var currentTop = $(window).scrollTop(),
-                    $catalog = $('.side-catalog');
+                var currentTop = $(window).scrollTop();
                 //check if user is scrolling up
                 if (currentTop < this.previousTop) {
                     //if scrolling up...
@@ -62,14 +60,6 @@ jQuery(document).ready(function($) {
                     if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
                 }
                 this.previousTop = currentTop;
-
-                //adjust the appearance of side-catalog
-                $catalog.show()
-                if (currentTop > (bannerHeight + 41)) {
-                    $catalog.addClass('fixed')
-                } else {
-                    $catalog.removeClass('fixed')
-                }
             });
     }
 });
